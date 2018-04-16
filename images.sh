cbaf189089c728d1b0740dc36ca6642b4ddf966d
Paweł Krupa <paulfantom@gmail.com>
2018-04-17 00:12:16 +0200

    copyright notice

--- images.sh
+++ images.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #
+# Copyright (C) 2018 Pawel Krupa (@paulfantom) - All Rights Reserved
+# Permission to copy and modify is granted under the MIT license
+#
 # Script downloads latest docker images from https://github.com/paulfantom/dockerfiles
+#
 # Requirements:
 #   - docker
-#
-# Author: Pawel Krupa (@paulfantom)
 #
 
 for i in ubuntu-molecule:18.04 ubuntu-molecule:16.04 debian-molecule:9 debian-molecule:8 centos-molecule:7 fedora-molecule:27
